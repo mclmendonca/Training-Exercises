3760dd8e023110e9640004fcfb3c5229a93fdf2e
mclmendonca <mfsm02@gmail.com>
2022-07-20 00:19:26 -0300

    update

--- Vetores/C/abaixo_da_media/main.c
+++ Vetores/C/abaixo_da_media/main.c
@@ -9,16 +9,29 @@ int main()
     printf("Quantos elementos vai ter o vetor? ");
     scanf("%d", &n);
 
-    double vet[n], soma = 0.0, cont = 0.0;
+    double vet[n], soma = 0.0, cont = 0.0, media;
 
-    for(i = 0; i < n; i++) {
-       printf("Digite um numero: ");
-       scanf("%lf", &vet[i]);
-       soma = soma + vet[i]
-       cont =
+    for(i = 0; i < n; i++)
+    {
+        printf("Digite um numero: ");
+        scanf("%lf", &vet[i]);
+        soma = soma + vet[i];
+        cont = cont + 1.0;
 
     }
+    media = soma/cont;
 
+    printf("\nMEDIA DO VETOR = %.3lf\n", media);
+    printf("ELEMENTOS ABAIXO DA MEDIA: \n");
+    for(i = 0; i < n; i++)
+    {
+        if(vet[i] < media)
+        {
+            printf("%.1lf\n", vet[i]);
+        }
+
+
+    }
 
 
     return 0;
