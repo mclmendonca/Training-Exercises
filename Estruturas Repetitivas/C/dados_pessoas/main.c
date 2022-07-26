ac2b559cad12ab6948aaf9758ab54c0612047a53
mclmendonca <mfsm02@gmail.com>
2022-07-25 22:26:14 -0300

    exercices

---
+++ Estruturas Repetitivas/C/dados_pessoas/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void limpar_entrada()
+{
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main()
+{
+    int i, n, nhomens = 0;
+
+    printf("Quantas pessoas serao digitadas? ");
+    scanf("%d", &n);
+
+    double altura[n], menor = 3.0, maior = 0.0, mediam = 0.0, contm = 0.0;
+    char genero[n];
+
+    for( i = 0 ; i < n ; i++)
+    {
+        printf("Altura da %da pessoa: ", i+1);
+        scanf("%lf", &altura[i]);
+        printf("Genero da %da pessoa: ", i+1);
+        limpar_entrada();
+        scanf("%c", &genero[i]);
+    }
+    if(genero[i] == 'F' || genero[i] == 'f')
+    {
+        mediam = mediam + altura[i];
+        contm = contm +1;
+    }
+    if(altura[i] < menor )
+    {
+        menor = altura[i];
+    }
+    if(altura[i]> maior)
+    {
+        maior = altura[i];
+    }
+    if(genero[i] == 'M' || genero[i] == 'm')
+    {
+        nhomens = nhomens + 1;
+    }
+
+    printf("Menor altura = %.2lf\n", menor);
+    printf("Maior altura = %.2lf\n", maior);
+    printf("Media das alturas das mulheres = %.2lf\n", mediam/contm);
+    printf("Numero de homens = %d\n", nhomens);
+
+    return 0;
+}
--- Vetores/C/aprovados/main.c
+++ Vetores/C/aprovados/main.c
@@ -6,11 +6,30 @@ int main()
     int i, n;
 
     printf("Quantos alunos serao digitados? ");
-    scanf("%d". &n);
+    scanf("%d", &n);
 
     char nome[n][50];
-    double nota1[n], nota2[n], media
+    double nota1[n], nota2[n], media[n];
 
-    Digite nome, primeira e segunda nota do 1o aluno:
+    for(i = 0; i < n; i++)
+    {
+        printf("Digite nome, primeira e segunda nota do %do aluno:\n", i+1);
+        fseek(stdin,0,SEEK_END);
+        gets(nome[i]);
+        scanf("%lf", &nota1[i]);
+        scanf("%lf", &nota2[i]);
+        media[i] = (nota1[i] + nota2[i])/2;
+
+    }
+    printf("Alunos aprovados: \n");
+
+    for(i = 0; i < n; i++)
+    {
+        if(media[i] >= 6.0 )
+        {
+            printf("%s\n", nome [i]);
+        }
+    }
+
     return 0;
 }
